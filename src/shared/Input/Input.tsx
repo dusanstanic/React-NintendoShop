cd58e9e88f26bbeb61da779c0a4b8c2affda6015
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-09-08 07:55:30 +0200

    Update InputCheckBox

--- src/shared/Input/Input.tsx
+++ src/shared/Input/Input.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-const InputCheckBox = (props: any) => {
+const InputCheckBox = (props: { name: string; value: any; click: any }) => {
   return (
     <div>
       <input
         name={props.name}
         type="checkbox"
         value={props.value}
-        onClick={(event) => props.updateDisplayedGames(event)}
+        onClick={(event) => props.click(event)}
       ></input>
       <label>{props.value}</label>
     </div>
