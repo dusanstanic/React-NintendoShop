0d6ee6a55bdd44262d698f51d1e91ebdf6308706
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-09-09 14:14:25 +0200

    Update Input

--- src/shared/Input/Input.tsx
+++ src/shared/Input/Input.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
-const InputCheckBox = (props: { name: string; value: any; click: any }) => {
+import classes from "./Input.module.css";
+
+const InputCheckBox = (props: {
+  name: string;
+  value: any;
+  click: any;
+  text: string;
+}) => {
   return (
     <div>
       <input
@@ -9,9 +16,28 @@ const InputCheckBox = (props: { name: string; value: any; click: any }) => {
         value={props.value}
         onClick={(event) => props.click(event)}
       ></input>
+      <label className={classes.label}>{props.text}</label>
+    </div>
+  );
+};
+
+const InputRadioBox = (props: {
+  name: string;
+  value: any;
+  click: any;
+  text: string;
+}) => {
+  return (
+    <div>
+      <input
+        name={props.name}
+        type="radio"
+        value={props.value}
+        onClick={(event) => props.click(event)}
+      ></input>
       <label>{props.value}</label>
     </div>
   );
 };
 
-export { InputCheckBox };
+export { InputCheckBox, InputRadioBox };
