f519c453537251d1234ac1312906cdc8926d6564
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-09-23 20:43:02 +0200

    Update gameData

--- src/store/reducers/gameData.tsx
+++ src/store/reducers/gameData.tsx
@@ -9,22 +9,26 @@ export interface GameDataState {
   games: Game[];
   genres: GenreM[];
   pgRatings: string[];
+  error: boolean;
 }
 
 const initialState: GameDataState = {
   games: [],
   genres: [],
   pgRatings: [],
+  error: false,
 };
 
 const reducer = (state = initialState, action: ActionTypes) => {
   switch (action.type) {
     case actionTypes.SET_GAMES:
-      return updateObject(state, { games: action.payload.games });
+      return updateObject(state, { games: action.payload.games, error: false });
     case actionTypes.SET_GENRES:
       return updateObject(state, { genres: action.payload.genres });
     case actionTypes.SET_PEGIRATINGS:
       return updateObject(state, { pgRatings: action.payload.pgRatings });
+    case actionTypes.FETCH_GAMES_FAILED:
+      return updateObject(state, { error: true });
     default:
       return state;
   }
