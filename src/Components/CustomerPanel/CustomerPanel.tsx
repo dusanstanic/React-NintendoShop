40345ef957df50c564ff60a9d34fa8897f6038a1
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-09-23 13:07:19 +0200

    Add CustomerPanel and style

---
+++ src/Components/CustomerPanel/CustomerPanel.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+
+const CustomerPanel: FunctionComponent = (props) => {
+  return (
+    <div style={{ margin: "100px", height: "5rem", background: "black" }}>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default CustomerPanel;
