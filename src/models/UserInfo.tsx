5f89805ec20a47ce21ef9d0b876cabec978d7e12
Dusan Stanic <dusan.stanic97@hotmail.com>
2020-10-06 12:58:44 +0200

    Add UserInfo model

---
+++ src/models/UserInfo.tsx
@@ -0,0 +1,11 @@
+export interface UserInfo {
+  firstName: string;
+  lastName: string;
+  city: string;
+  street: string;
+  streetNumber: string;
+  phone: string;
+  postalCode: number;
+  email: string;
+  gender: string;
+}
