99b5971ee6cae6c7544e76d773d93959dce195e3
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-02-09 15:27:15 +0100

    Add scss to Game

--- src/Components/Games/Game/Game.tsx
+++ src/Components/Games/Game/Game.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import classes from "./Game.module.css";
+import classes from "./Game.module.scss";
 
 import Aux from "../../../hoc/Auxiliary";
 
@@ -9,38 +9,42 @@ import GamesM from "../../../models/GameM";
 const Game = (props: { game: GamesM; showGame: any }) => {
   return (
     <Aux>
-      <div className={classes["game-tile"]}>
-        <div className={classes["game-image-wrapper"]}>
+      <div className={classes["product__tile"]}>
+        <div className={classes["product__wrapper"]}>
           <img
-            alt="Nintendo Game"
-            src={props.game.images ? props.game.images[0].destination : ""}
-            className={classes["game-tile-image"]}
+            alt="Nintendo Product"
+            src={props.game.images && props.game.images[0].destination}
+            className={classes["product__image"]}
           ></img>
-          <div className={classes["game-caption-icons"]}>
+          <div className={classes["caption"]}>
             <div
-              className={classes["game-caption-icon-show"]}
+              className={`${classes["caption__icon"]} ${classes["caption__icon--show"]}`}
               onClick={props.showGame}
             ></div>
-            <div className={classes["game-caption-icon-compare"]}></div>
-            <div className={classes["game-caption-icon-favorite"]}></div>
+            <div
+              className={`${classes["caption__icon"]} ${classes["caption__icon--compare"]}`}
+            ></div>
+            <div
+              className={`${classes["caption__icon"]} ${classes["caption__icon--favorite"]}`}
+            ></div>
           </div>
         </div>
         <img
-          className={classes["game-logo"]}
+          className={classes["product__logo"]}
           alt="logo"
           src={props.game.consoles ? props.game.consoles[0].logo : ""}
         />
-        <div className={classes["game-tile-release-date-wrapper"]}>
-          <div className={classes["game-tile-release-date"]}>
+        <div className={classes["product__date-wrapper"]}>
+          <div className={classes["product__date"]}>
             Release Date: {props.game.releaseDate.toLocaleDateString()}
           </div>
         </div>
-        <h4 className={classes["game-tile-title"]}>{props.game.title}</h4>
-        <div className={classes["game-tile-price-item"]}>
-          <div className={classes["game-tile-condition"]}>
+        <h4 className={classes["product__title"]}>{props.game.title}</h4>
+        <div className={classes["price-item"]}>
+          <div className={classes["price-item__condition"]}>
             {props.game.status}
           </div>
-          <div className={classes["game-tile-price"]}>
+          <div className={classes["price-item__price"]}>
             {props.game.price}
             <span
               style={{
@@ -52,9 +56,9 @@ const Game = (props: { game: GamesM; showGame: any }) => {
               <span style={{ fontSize: "9px", display: "block" }}>RSD</span>
             </span>
           </div>
-          <button className={classes["game-add-to-cart-btn"]}></button>
+          <button className={classes["price-item__cart"]}></button>
         </div>
-        <div className={classes["game-tile-banner"]}>PreOrder</div>
+        <div className={classes["product__banner"]}>PreOrder</div>
       </div>
     </Aux>
   );
