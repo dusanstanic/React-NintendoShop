bd08e971156a85cd620851d07e72364075c567b7
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-02-12 14:25:25 +0100

    Update UI components

--- src/shared/UI/Input/Input.tsx
+++ src/shared/UI/Input/Input.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { FunctionComponent, useState } from "react";
+import { useStore } from "react-redux";
 
 import classes from "./Input.module.css";
 
@@ -7,6 +8,8 @@ const InputCheckBox = (props: {
   value: any;
   click: any;
   text: string;
+  checked?: boolean;
+  class?: string;
 }) => {
   return (
     <div>
@@ -14,7 +17,9 @@ const InputCheckBox = (props: {
         name={props.name}
         type="checkbox"
         value={props.value}
-        onClick={(event) => props.click(event)}
+        onChange={(event) => props.click(event)}
+        checked={props.checked}
+        className={props.class}
       ></input>
       <label className={classes.label}>{props.text}</label>
     </div>
@@ -40,4 +45,82 @@ const InputRadioBox = (props: {
   );
 };
 
-export { InputCheckBox, InputRadioBox };
+interface PropsInputCheckbox {
+  name: string;
+  value: any;
+  click: any;
+  checked?: boolean;
+  className?: string;
+  isValid?: string;
+}
+
+const InputCheckBox1: FunctionComponent<PropsInputCheckbox> = ({
+  name,
+  className,
+  click,
+  value,
+  checked,
+  children,
+  isValid,
+}) => {
+  const cssClasses = [
+    className,
+    isValid ? classes["valid"] : classes["invalid"],
+  ];
+
+  return (
+    <input
+      name={name}
+      type="checkbox"
+      value={value}
+      onChange={(event) => click(event)}
+      checked={checked}
+      className={cssClasses.join(" ")}
+    ></input>
+  );
+};
+
+interface PropsIInput {
+  type: string;
+  name: string;
+  placeholder?: string;
+  value: any;
+  change: any;
+  checked?: boolean;
+  className?: string;
+  validClassName?: string;
+  invalidClassName?: string;
+  isValid?: boolean;
+}
+
+const Input: FunctionComponent<PropsIInput> = ({
+  type,
+  name,
+  placeholder,
+  value,
+  change,
+  className,
+  validClassName,
+  invalidClassName,
+  checked,
+  isValid,
+}) => {
+  const cssClasses = [className, isValid ? validClassName : invalidClassName];
+
+  if (type === "text") {
+    return (
+      <input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(event) => change(event)}
+        className={cssClasses.join(" ")}
+      ></input>
+    );
+  }
+
+  return <div></div>;
+};
+
+export { InputCheckBox, InputRadioBox, Input, InputCheckBox1 };
